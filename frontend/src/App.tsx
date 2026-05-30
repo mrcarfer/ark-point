9ed47173cedb54293afdd32370cdb8db70145558
Leandro <leeandroocarvalhoo@gmail.com>
2026-05-30 18:44:45 -0300

    organiza estrutura inicial frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div>
-      <h1>ARK Point</h1>
-      <p>Sistema inteligente de controle de jornada</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>ARK Point</h1>
+      <p>Sistema inteligente de controle de jornada</p>
+    </div>
+  );
+}
+
+export default Home;
