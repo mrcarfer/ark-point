045c41cc66b181ec8aeffa7d305f1bf12dfd9cb3
Leandro <leeandroocarvalhoo@gmail.com>
2026-05-31 14:37:03 -0300

    implementa cadastro de usuario

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(data: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const company = await this.prisma.company.create({
+      data: {
+        name: 'Empresa Padrão',
+      },
+    });
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+        role: 'ADMIN',
+        companyId: company.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        companyId: true,
+        createdAt: true,
+      },
+    });
+
+    return user;
+  }
+}
